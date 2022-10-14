d9de8ba5106800263cb2a33b28c71c30c523dcb1
Piotr Madura <ppiotr.madura@gmail.com>
2022-10-14 15:17:08 +0200

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p class="hover:text-cyan-300 peer-hover:underline">test</p>
+
+<button class="btn">DaisyUI</button>
